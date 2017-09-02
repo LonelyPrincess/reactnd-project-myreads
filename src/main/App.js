fe6295a6b23f9abdda743156a76987367d17dd56
Sara <sara.her.su@gmail.com>
2017-09-02 19:11:03 +0100

    feat: Navigation handled by React Router
    The 'showSearchPage' property inside of the App's component state has been
    removed. Instead, navigation from and to the search page is now being
    handled with React Router.

--- src/main/App.js
+++ src/main/App.js
@@ -1,18 +1,13 @@
 import React from 'react'
-import * as BooksAPI from './utils/BooksAPI'
+import { Route } from 'react-router-dom'
+
 import '../res/styles/App.css'
 
-import BookListItem from './components/BookListItem';
+import * as BooksAPI from './utils/BooksAPI'
+import BookListItem from './components/BookListItem'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
     books: []
   }
 
@@ -25,28 +20,9 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
+        { /* Book list page */ }
+        <Route exact path='/' render={({ history }) => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -86,10 +62,34 @@ class BooksApp extends React.Component {
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <a onClick={() => history.push('/search')}>Add a book</a>
             </div>
           </div>
-        )}
+        )} />
+
+        { /* Search books page */ }
+        <Route path='/search' render={({ history }) => (
+          <div className="search-books">
+            <div className="search-books-bar">
+              <a className="close-search" onClick={() => history.push('/')}>Close</a>
+              <div className="search-books-input-wrapper">
+                {/*
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+                <input type="text" placeholder="Search by title or author"/>
+
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+            </div>
+          </div>
+        )} />
       </div>
     )
   }

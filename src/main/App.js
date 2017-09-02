c3f9c836ef1ddec4c14eef76ca67dd4ba989df11
Sara <sara.her.su@gmail.com>
2017-09-02 12:34:22 +0100

    refactor: Update in project folder structure
    In order to make it easier to manage the code as the app starts to grow, I
    created new folders to reorganize the files inside of the 'src' directory.
    
    In the new structure:
    
     - The 'main' folder will contain the code responsible for making the
       application work.
    
     - The unit tests will be under a separated 'test' folder, so they're not
       mixed up with our actual application code.
    
     - The 'res' folder will happen to contain stylesheets, images or any other resources our
    application will need to use.

--- src/main/App.js
+++ src/main/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
-import './App.css'
+// import * as BooksAPI from './utils/BooksAPI'
+import '../res/styles/App.css'
 
 class BooksApp extends React.Component {
   state = {

bdde0fb8974c89bed74d364dfc6335a563fc8f3d
Sara <sara.her.su@gmail.com>
2017-09-10 17:12:37 +0100

    test: New tests for the `App` component
    Two new tests are now included to the `App.test.js` file. The two of them
    are related to the header:
    
     - Check if the header is rendered and contains the app name.
    
     - If the current location if the root path, ensure the rendered header
       contains no back button.

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,16 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import { BrowserRouter } from 'react-router-dom';
 
-import App from '../main/App'
+import App from '../main/App';
 
-/**
- This course is not designed to teach Test Driven Development.
- Feel free to use this file to test your application, but it
- is not required.
-**/
+describe('\n --- TESTS FOR APP COMPONENT --- \n', () => {
+  let wrapper;
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, div)
-})
+  // Initialize wrapper before running any of the tests
+  beforeAll(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, div);
+  });
+
+  it('renders a header with the application name', () => {
+    const header = wrapper.find('header');
+    expect(header).toHaveLength(1);
+    expect(header.find('h1').text()).toBe('MyReads');
+  });
+
+  it('renders a header without back button in root path', () => {
+
+    // TODO: find another way to retrieve current path from react router
+    const currentPath = window.location.pathname;
+    if (currentPath !== '/') {
+      return;
+    }
+
+    const header = wrapper.find('header.without-back-button');
+    expect(header).toHaveLength(1);
+    expect(header.children()).toHaveLength(2);
+    expect(header.find('.back-button')).toHaveLength(0);
+  });
+});

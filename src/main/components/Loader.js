b40455614ab652f9ae32daa42ec0dcbc5d622c39
Sara <sara.her.su@gmail.com>
2017-09-11 17:58:34 +0100

    refactor + design: Improvements in loader styles
    The loader elements do now have the three main colors used on the app's
    layout: light green, orange and dark gray.
    
    Both .css and .js files for this component have been reviewed and update
    with cleaner code.

--- src/main/components/Loader.js
+++ src/main/components/Loader.js
@@ -11,16 +11,14 @@ import '../../res/styles/Loader.css';
  * @author LonelyPrincess <sara.her.su@gmail.com>
  */
 function Loader () {
+  const message = "Loading";
+
   return (
     <div className="loader-container">
       <div className="loader-body">
-        <span className="loader-letter-l">L</span>
-        <span className="loader-letter-o">o</span>
-        <span className="loader-letter-a">a</span>
-        <span className="loader-letter-d">d</span>
-        <span className="loader-letter-i">i</span>
-        <span className="loader-letter-n">n</span>
-        <span className="loader-letter-g">g</span>
+        {message.split("").map((letter, index) => (
+          <span key={index}>{letter}</span>
+        ))}
       </div>
     </div>
   );

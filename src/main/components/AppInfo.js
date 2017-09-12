770d351760b8f8c04239b1274bc223589985b83f
Sara <sara.her.su@gmail.com>
2017-09-12 16:14:33 +0100

    refactor + design: Minor adjustments in design
    Along with the review of all of the available css code, I made a couple of
    adjustments to the design (such as fixing the search input at the top
    of the page, so it doesn't dissappear when the user scrolls down the page
    to see the results).
    
    Some templates were also updated to work correctly with the new version of
    the styles.
    
    Since the stylesheet is pretty long right now, it may be a good idea to
    split it into more smaller parts in future updates.

--- src/main/components/AppInfo.js
+++ src/main/components/AppInfo.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import '../../res/styles/AppInfo.css';
-
 /**
  * Stateless component to render static information about this application.
  *
@@ -19,8 +17,7 @@ function AppInfo() {
 
       <section>
         <h2>About 'MyReads'</h2>
-        <p>The aim of this application is to allow the user to keep track of their own personal library. It includes a list
-          of the books that are already in their possession or those in their wishlist.</p>
+        <p>The aim of this application is to allow the user to keep track of their own personal library. It includes a list of the books that are already in their possession or those in their wishlist.</p>
         <p>Each of these books will be grouped in three shelves depending on their status:</p>
 
         <ul>
@@ -29,28 +26,20 @@ function AppInfo() {
           <li>Read</li>
         </ul>
 
-        <p>The user will be able to change a book status anytime, as well as add new titles to their collection or remove items
-          from their library.</p>
+        <p>The user will be able to change a book status anytime, as well as add new titles to their collection or remove items from their library.</p>
       </section>
 
       <section>
         <h2>About the project</h2>
-        <p>This application was built by using <a href="https://facebook.github.io/react/" target="_blank">React.js</a>, and serves as the
-         final project for the first module of the <a href="https://www.udacity.com/course/react-nanodegree--nd019" target="_blank">Udacity's React nanodegree</a>,
-         <em>"React Fundamentals"</em>.</p>
-        <p>If you like what you're seeing, you can view the source code at <a href="https://github.com/LonelyPrincess/reactnd-project-myreads"
-          target="_blank">project's repository</a> and support this work by staring it or following the <a href="https://github.com/LonelyPrincess"
-          target="_blank">author</a> in <a href="https://github.com/" target="_blank">GitHub</a>.</p>
+        <p>This application was built by using <a href="https://facebook.github.io/react/" target="_blank">React.js</a>, and serves as the final project for the first module of the <a href="https://www.udacity.com/course/react-nanodegree--nd019" target="_blank">Udacity's React nanodegree</a>, <em>"React Fundamentals"</em>.</p>
+        <p>If you like what you're seeing, you can view the source code at <a href="https://github.com/LonelyPrincess/reactnd-project-myreads" target="_blank">project's repository</a> and support this work by staring it or following the <a href="https://github.com/LonelyPrincess" target="_blank">author</a> in <a href="https://github.com/" target="_blank">GitHub</a>.</p>
       </section>
 
       <section>
         <h2>About the developer</h2>
         <p>This little project was created by an spanish girl called Sara, also known in the web by the nicknames <i>LonelyPrincess</i> and <i>Natsuko Kuroigawa</i>.</p>
-        <p>Currently working as an hybrid mobile developer, her true passion lies in web development and enrolled in this nanodegree
-          to get hands-on React and keep on improving her skills as a full-stack web developer.</p>
-        <p>She's also an avid fan of videogames (especially japanese rpgs and survival horror), and other hobbies of hers include
-          reading, watching anime series, writing and listening to music while programming. As you have probably
-          noticed by now, she has a soft-spot for cats.</p>
+        <p>Currently working as an hybrid mobile developer, her true passion lies in web development and enrolled in this nanodegree to get hands-on React and keep on improving her skills as a full-stack web developer.</p>
+        <p>She's also an avid fan of videogames (especially japanese rpgs and survival horror), and other hobbies of hers include reading, watching anime series, writing and listening to music while programming. As you have probably noticed by now, she has a soft-spot for cats.</p>
 
         <p>For more information, feel free to check any of these online profiles:</p>
 

962dc48819844d86519863ea230b1855032a514c
Sara <sara.her.su@gmail.com>
2017-09-16 10:25:36 +0100

    test: Split book list item tests in smaller ones
    The test for checking if the book list item was rendered with the right
    data has been split into smaller pieces, one for each of the displayed
    data elements: author list, title, link to details page, thumbnail and
    book shelf.

--- src/test/components/BookListItem.test.js
+++ src/test/components/BookListItem.test.js
@@ -17,19 +17,35 @@ describe('\n --- TESTS FOR BOOK LIST ITEM COMPONENT --- \n', () => {
 
   const book = data.book;
 
-  it('render book data', () => {
-    const wrapper = getComponentWrapper(
+  let wrapper;
+
+  beforeAll (() => {
+    wrapper = getComponentWrapper(
       <BookListItem book={book} onShelfChange={() => null} />
     );
+  });
 
+  it('renders book title', () => {
     expect(wrapper.find('.book-title').text())
       .toBe(book.title);
+  });
+
+  it('renders book author list', () => {
     expect(wrapper.find('.book-authors').text())
       .toBe(book.authors.join(', '));
+  });
+
+  it('renders link to book details page', () => {
     expect(wrapper.find('a').prop('href'))
       .toBe(`/details/${book.id}`);
+  });
+
+  it('renders book thumbnail', () => {
     expect(wrapper.find('.book-cover').prop('style').backgroundImage)
       .toBe(`url('${book.imageLinks.thumbnail}')`);
+  });
+
+  it('initializes book shelf selector with current shelf value', () => {
     expect(wrapper.find('select').prop('value'))
       .toBe(book.shelf);
   });
